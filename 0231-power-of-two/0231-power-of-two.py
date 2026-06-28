5f4515b9ca8a378c2934b4654d59394ec903ccb7
Muhammad Usman Khan <m.usman.khan.stu@gmail.com>
2026-06-28 14:13:48 +0500

    Time: 8 ms (3.31%), Space: 12.4 MB (53.89%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        for i in range(50):
+            if n == 2**i:
+                return True
+        return False
