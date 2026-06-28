61a6e2b5dd4aa629848de52b4125b309969385ae
Muhammad Usman Khan <m.usman.khan.stu@gmail.com>
2026-06-28 14:26:12 +0500

    Time: 0 ms (100%), Space: 12.4 MB (17.43%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        while num >= 10:
+            total = 0
+            for digit in str(num):
+                total += int(digit)
+
+            num = total 
+
+        return num
